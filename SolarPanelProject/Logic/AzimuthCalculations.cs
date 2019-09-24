f3194a17cce8bbc572cdb11a103ce95b2ec4f342
Kamil Gretka <infermus96@gmail.com>
2019-09-24 11:00:05 +0200

    Sun azimuth calc implementation

--- SolarPanelProject/Logic/AzimuthCalculations.cs
+++ SolarPanelProject/Logic/AzimuthCalculations.cs
@@ -4,9 +4,24 @@ namespace SolarPanelProject.Logic
 {
     internal class AzimuthCalculations
     {
+    /// <summary>
+    /// Returns current sun azimuth in degrees
+    /// </summary>
+    /// <param name="latitude"></param>
+    /// <param name="sunAltitude"></param>
+    /// <param name="magneticDeclination"></param>
+    /// <param name="hourAngle"></param>
+    /// <returns></returns>
         internal double CalculateSunAzimuth(float latitude, double sunAltitude, double magneticDeclination, double hourAngle)
         {
-            throw new NotImplementedException();
+            Helpers helpers = new Helpers();
+
+            return helpers.ConvertRadiansToDegree(Math.Acos((Math.Sin(helpers.ConvertDegreeToRadians(magneticDeclination)) * 
+                             Math.Cos(helpers.ConvertDegreeToRadians(latitude)) - 
+                             (Math.Cos(helpers.ConvertDegreeToRadians(magneticDeclination)) * 
+                             Math.Sin(helpers.ConvertDegreeToRadians(latitude)) * 
+                             Math.Cos(helpers.ConvertDegreeToRadians(hourAngle)))) 
+                             / Math.Cos(helpers.ConvertDegreeToRadians(sunAltitude))));
         }
     }
 }
